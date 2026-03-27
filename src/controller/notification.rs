1a862699b2500cfaa774d361d544bf2d895dc884
Rifqi Ilham <muhammad.rifqi411@ui.ac.id>
2026-03-27 23:06:48 +0700

    Implement subscribe function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -4,3 +4,11 @@ use bambangshop_receiver::Result;
 use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
 use crate::service::notification::NotificationService;
+
+#[get("/subscribe/<product_type>")]
+pub fn subscribe(product_type: String) -> Result<Json<SubscriberRequest>> {
+    match NotificationService::subscribe(&product_type) {
+        Ok(x) => Ok(Json::from(x)),
+        Err(e) => Err(e),
+    }
+}
